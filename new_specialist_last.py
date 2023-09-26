fb06f97fee9e363b401a2db4fa209fdd21498b7d
Gid-Git <gidon.quint@ziggo.nl>
2023-09-26 12:16:44 +0200

    added and tested bit mutation

--- new_specialist_last.py
+++ new_specialist_last.py
@@ -89,10 +89,17 @@ class EvoMan:
         return fitness, health_gain, time_game
     
     def mutate(self, individual):
-        # Applies mutation to the individual based on the mutation rate
+        # Applies bit mutation to the individual based on the mutation rate
         for i in range(len(individual)):
             if random.uniform(0, 1) < self.mutation_rate:
-                individual[i] = np.random.normal(self.dom_l, self.dom_u)
+                # Convert the weight to binary representation
+                binary_representation = list(format(int((individual[i] - self.dom_l) * (2**15) / (self.dom_u - self.dom_l)), '016b'))
+                
+                for j in range(len(binary_representation)):
+                    if random.uniform(0, 1) < self.mutation_rate:
+                        binary_representation[j] = '1' if binary_representation[j] == '0' else '0'
+                
+                individual[i] = int("".join(binary_representation), 2) * (self.dom_u - self.dom_l) / (2**15) + self.dom_l
         return individual
     
     def crossover(self, parent1, parent2):
